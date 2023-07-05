06b9a8d67ca871d178ca759293271d8d03fbb449
jinseok <wlstjrghdud@naver.com>
2023-07-05 23:48:26 +0900

    design: 레이아웃 정리

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,20 +1,9 @@
 import MarkdownEditor from '@/components/MarkdownEditor'
 
-// const MDEditor = dynamic(
-//   () => import("@uiw/react-md-editor"),
-//   { ssr: false }
-// );
-
-// const EditerMarkdown = dynamic(() =>
-//   import('@uiw/react-md-editor').then(mod => {
-//     return mod.default.Markdown
-//   })
-// )
-
-export default async function PostDetail() {
+export default async function CreatePost() {
   return (
-    <div data-color-mode="dark">
-      <MarkdownEditor />{' '}
+    <div>
+      <MarkdownEditor />
     </div>
   )
 }
