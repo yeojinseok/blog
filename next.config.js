106a14fefceceb6728b7bdaa77fe9dc08a279a5f
jinseok <wlstjrghdud@naver.com>
2023-07-20 01:30:04 +0900

    design: create post 디자인 진행중

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['cdn.sanity.io'],
+    domains: [
+      'cdn.sanity.io',
+      'jindolog-images.s3.ap-northeast-2.amazonaws.com',
+    ],
   },
 }
 
--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,8 +1,14 @@
-import MarkdownEditor from '@/components/MarkdownEditor'
+import dynamic from 'next/dynamic'
+import { Suspense } from 'react'
+
+const MarkdownEditor = dynamic(
+  () => import('@/components/MarkdownEditor').then(mod => mod.default),
+  { ssr: false, loading: () => <div>loading</div> }
+)
 
 export default async function CreatePost() {
   return (
-    <div>
+    <div className=" w-full h-full">
       <MarkdownEditor />
     </div>
   )
